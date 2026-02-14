3346be7f1fa0e9ca26d4f19b210845f44c1f74a7
Vignesh877845 <vicky877845@gmail.com>
2026-02-14 17:28:38 +0530

    feat: implement UUID users and user_credentials authentication structure.

---
+++ database/migrations/2026_02_14_115323_create_user_credentials_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_credentials', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->string('provider')->default('local');
+            $table->string('provider_id')->nullable();
+            $table->string('password')->nullable();
+            $table->timestamp('last_login_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_credentials');
+    }
+};
