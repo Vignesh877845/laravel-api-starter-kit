147651b878b18820b754a90bb65e72dcc88332a8
Vignesh877845 <vicky877845@gmail.com>
2026-02-14 14:16:45 +0530

    fix: correct Request type-hint in exception handler.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
