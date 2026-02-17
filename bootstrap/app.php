d533405c02c2b857c9bdd40d3569c2f98f98ebbf
vignesh8526 <vignesh.bnss@dmifoundations.org>
2026-02-17 10:29:16 +0530

    feat(auth): implement separate authentication handling for web and API routes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,19 +10,35 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+
+        $middleware->redirectGuestsTo(function (Request $request){
+            if($request->is('api/*')){
+                return null;
+            }
+            return route('login');
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unauthenticated'
+                ], 401);
+            }
+        });
+
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if($request->is('api/*')){
+            if ($request->is('api/*')) {
                 return response()->json([
                     'status' => false,
                     'message' => 'Resource not found'
@@ -29,9 +46,8 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
 
-
-        $exceptions->render(function (ValidationException $e, Request $request){
-            if($request->is('api/*')){
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            if ($request->is('api/*')) {
                 return response()->json([
                     'status' => false,
                     'message' => 'Validation error',
