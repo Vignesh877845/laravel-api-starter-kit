bd2d89952e122af4753ac345e3fc666b1ac56690
vignesh8526 <vignesh.bnss@dmifoundations.org>
2026-02-23 16:04:42 +0530

    refactor: remove multi-tenancy feature to keep the starter kit lightweight.

--- config/features.php
+++ config/features.php
@@ -1,6 +1,5 @@
 <?php 
 
 return [
-    'roles_permission' => env('ROLES_PERMISSION', true),
-    'multi_tenancy' => env('MULTI_TENANCY', false),
+    'roles_permission' => env('ROLES_PERMISSION', true)
 ];
